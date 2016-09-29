ab4cff860e1d797738b6bb4f5001b96eb32550b8
Ilya Ivanov <renge_ten@yahoo.com>
2016-09-29 19:30:28 +0300

    Bugfix

--- homework/new_homework/bot.py
+++ homework/new_homework/bot.py
@@ -6,7 +6,7 @@ def answer(user_text):
         return "dont shout on me!"
     if "?" in user_text:
         return "I would not answer any questions without my lawyer"
-    dictionary = {'hello':'Hello!', "bye":"Bye!", "how are you?"}
+    dictionary = {'hello':'Hello!', "bye":"Bye!"}
     usrmsg = dictionary.get(user_text, "I dont know!")
     return usrmsg
 
